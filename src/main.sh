b214ac305fb33b56f60be5d8ed36da5539008e48
francescobianco <info.francescobianco@gmail.com>
2023-11-07 21:03:04 +0100

    First commit

--- src/main.sh
+++ src/main.sh
@@ -1,6 +1,14 @@
 
+usage() {
+    echo "The command linter"
+    echo "  Usage: cmdlint [COMMAND]"
+    echo "  Run check on output of COMMAND in common shell use cases."
+    exit
+}
+
 main() {
   [ $# -eq 0 ] && usage
+  [ "$1" = "--help" ] && usage
 
   echo "Hello World!"
 }
