f104a2babd032299990f4d99a4697f0e02cbac02
francesco <info.francescobianco@gmail.com>
2023-11-09 09:40:18 +0100

    First commit

--- src/rules/rule_01x.sh
+++ src/rules/rule_01x.sh
@@ -2,6 +2,7 @@
 cmdlint_rule_01x() {
     local command
     local output
+    local usage_line
 
     command=$1
 
@@ -12,7 +13,12 @@ cmdlint_rule_01x() {
             raise_error R011 "${command}" "Missing '--help' on usage message"
         fi
         if grep "^Usage:" "${output}.out" >/dev/null 2>&1; then
-            extract_usage_lines "${output}.out"
+            extract_usage_lines "${output}.out" | while IFS=$'\n' read -r usage_line; do
+              if ! echo "${usage_line}" | grep -q "^$command"; then
+                raise_error R012 "${command}" "Usage line does not start with '${command}'"
+                highlight_line "Usage:\n${usage_line}" 0 4 ""
+              fi
+            done
         fi
     fi
 }
